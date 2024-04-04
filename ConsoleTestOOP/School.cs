b94738fcf8b57aa4f14ece72acef5cefcb914249
Kail XY <ribozinka@gmail.com>
2024-04-04 22:43:10 +0300

    First classes for work a system

---
+++ ConsoleTestOOP/School.cs
@@ -0,0 +1,18 @@
+﻿using LibStudent;
+
+namespace CreateCshool
+{
+    internal class School
+    {
+        public string Name;
+        public List<Student> Students;
+
+        public School(string name)
+        {
+            Name = name;
+            Students = new List<Student>();
+        }
+    }
+    
+}
+
---
+++ ConsoleTestOOP/Student.cs
@@ -0,0 +1,18 @@
+﻿using CreateCshool;
+
+namespace LibStudent
+{
+    internal class Student
+    {
+        public string FirstName;
+        public string LastName;
+        public int Age;
+
+        public Student (string firstName, string lastName, int age)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+        }
+    }
+}
